61255a92a5e7fa793224f9bbe77e99df2433e654
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-12 11:42:33 +0200

    Add project files.

---
+++ testoBinario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace testoBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine(" inserisci una parola:");
+            s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                Console.WriteLine((int)c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
